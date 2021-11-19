60fcf91ada3b2f6ecc9af530d3093a80b63e92d2
김나윤 <belloyv@gmail.com>
2021-11-19 12:40:27 +0900

    Update HappyHouse6FinalSpringBootApplication.java

--- src/main/java/com/ssafy/happyhouse/HappyHouse6FinalSpringBootApplication.java
+++ src/main/java/com/ssafy/happyhouse/HappyHouse6FinalSpringBootApplication.java
@@ -1,8 +1,9 @@
 package com.ssafy.happyhouse;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.ssafy.happyhouse.model.dao")
 @SpringBootApplication
 public class HappyHouse6FinalSpringBootApplication {
 
